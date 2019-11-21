f05381358fd8cd08beeb32c5b127db873af99905
Gustavo <gsoter@live.com>
2019-11-21 16:36:38 -0300

    Implementação de suite de testes

---
+++ test/entidades/SuiteTesteUnitario.java
@@ -0,0 +1,11 @@
+package entidades;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AptoTest.class, ClienteTest.class })
+public class SuiteTesteUnitario {
+
+}
